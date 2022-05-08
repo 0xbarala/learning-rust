7b7cbf5054535a37fe39c990738a6017525d6905
insanebarala <Varun Barala>
2022-05-09 00:15:17 +0800

    enums in rust

---
+++ src/enums.rs
@@ -0,0 +1,31 @@
+//Enums are types which have a few definitve values
+
+enum Movement {
+    //Variants
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+fn move_avatar(m: Movement)
+{
+    match m {
+        Movement::Up => println!("Avatar moving Up!"),
+        Movement::Down => println!("Avatar moving Down!"),
+        Movement::Left => println!("Avatar moving Left!"),
+        Movement::Right => println!("Avatar moving Right!")
+    }
+}
+
+pub fn run() {
+    let avatar1 = Movement::Left;
+    let avatar2 = Movement::Right;
+    let avatar3 = Movement::Down;
+    let avatar4 = Movement::Up;
+
+    move_avatar(avatar1);
+    move_avatar(avatar2);
+    move_avatar(avatar3);
+    move_avatar(avatar4);
+}
